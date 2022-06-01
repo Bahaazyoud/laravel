9c7f8a49c88f6f456fbc01a48a592cc415a46be7
Bahaazyoud <bahaazyoud2000@gmail.com>
2022-06-01 07:30:08 +0300

    upload

---
+++ my-project/resources/views/task1.blade.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="resources/css/task1.css">
+</head>
+
+<body style="background-color: #4287f5;">
+    <form>
+        <section>
+            <div class="container ">
+                <div class="row d-flex justify-content-center align-items-center ">
+                    <div class="col">
+                        <div class="card card-registration my-4">
+                            <div class="row g-0">
+                                <div class="col-xl-6 d-none d-xl-block">
+                                    <img src="https://wallpaper.dog/large/10808900.jpg" alt="Sample photo" class="img-fluid" style="border-top-left-radius: .25rem; border-bottom-left-radius: .25rem;" />
+                                </div>
+                                <div class="col-xl-6">
+                                    <div class="card-body p-md-5 text-black">
+                                        <h3 class="mb-5 text-uppercase" style="text-align: center;">Register</h3>
+                                        <div class="form-outline mb-4">
+                                            <div class="form-outline mb-4">
+                                                <input type="text" id="form3Example1cg" class="form-control form-control-lg" placeholder="UserName" style="border:1px solid; border-radius: 10px;" />
+                                            </div>
+
+                                            <div class="form-outline mb-4">
+                                                <input type="email" id="form3Example3cg" class="form-control form-control-lg" placeholder="Email Address" style="border:1px solid; border-radius: 10px;" />
+                                            </div>
+
+                                            <div class="form-outline mb-4">
+                                                <input type="password" id="form3Example4cg" class="form-control form-control-lg" placeholder="Password" style="border:1px solid; border-radius: 10px;" />
+                                            </div>
+
+                                            <div class="form-outline mb-4">
+                                                <input type="password" id="form3Example4cdg" class="form-control form-control-lg" placeholder="Confirm Password" style="border:1px solid; border-radius: 10px;" />
+                                            </div>
+                                            <div class="d-flex justify-content-center">
+                                                <button type="button" class="btn btn-primary btn-block btn-lg" style="border-radius: 10px;">Register</button>
+                                            </div>
+                                            <br>
+                                            <a href="#!" class="fw-bold text-body" style="border-radius: 10px;">
+                                                <p style="text-align: center;">SignUP</p>
+                                            </a>
+                                            <hr>
+                                            <div class="form-outline mb-4">
+                                                <input type="submit" value="SIGN UP WITH GOOGLE" class="btn btn-danger btn-lg btn-block rounded-3 py-2 px-4 mb-3" style="border-radius: 10px;"><br>
+                                                <input type="submit" value="SIGN UP WITH FACEBOOK" class="btn btn-primary btn-lg btn-block active rounded-3 py-2 px-4 mb-3" style="border-radius: 10px;">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+        </section>
+    </form>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
